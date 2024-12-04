6bdb021a5a4cc6d1c7295cb5a347ef60e8b8b91e
Heru <qns7rq@virginia.edu>
2024-12-03 19:04:02 -0500

    added ip api to get info from ip of client

--- privacyproject/mainapp/views.py
+++ privacyproject/mainapp/views.py
@@ -1,7 +1,20 @@
+import json
+import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
     client_ip = request.META.get('REMOTE_ADDR')
-    return HttpResponse("Test Page. Client IP: %s" % client_ip)
+    ip_api_url = 'http://ip-api.com/json/%s' % client_ip
+    info_json = {}
+    try:
+        ip_response = requests.get(ip_api_url)
+
+        if ip_response.status_code == 200:
+            info_json = ip_response.json()
+            print(info_json)
+        else:
+            print('Error: ', ip_response.status_code)
+    finally:
+        return HttpResponse("Test Page. Client IP: %s" % client_ip)
