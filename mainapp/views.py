8ad8ec621e2428a136e6b2d28974cc6a38e0045e
Heru <qns7rq@virginia.edu>
2024-12-03 19:37:55 -0500

    changed ip request

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,7 +5,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    client_ip = request.META.get('REMOTE_ADDR')
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        client_ip = x_forwarded_for.split(',')[0]
+    else:
+        client_ip = request.META.get('REMOTE_ADDR')
     ip_api_url = 'http://ip-api.com/json/%s' % client_ip
     info_json = {}
     try:
